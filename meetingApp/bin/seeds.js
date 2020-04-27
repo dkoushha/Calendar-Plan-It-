bf9a7b5c9d3ef4affd8615184be92e3b73ce67f4
dkoushha <dimakoushha@gmail.com>
2020-04-27 16:57:58 +0200

    create the app.

---
+++ meetingApp/bin/seeds.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const User = require('../models/user');
+
+mongoose.connect(`mongodb://localhost/meetingApp`, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const users = [{
+    username: "dima",
+    password: "12345",
+
+}];
+
+User.create(users).then(() => {
+    console.log(`Created ${users.length} user`);
+    mongoose.connection.close();
+});
