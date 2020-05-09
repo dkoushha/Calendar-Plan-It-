6683395eb0d241733e8915c4fea881595543718d
dkoushha <dimakoushha@gmail.com>
2020-05-09 11:23:50 +0200

    fix some bugs with the reminder

--- meetingApp/app.js
+++ meetingApp/app.js
@@ -142,59 +142,56 @@ const personalAccount = require("./routes/personalAccount.routes");
 app.use("/", personalAccount);
 
 
-// email authorization
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.GMAIL_USERNAME,
-//     pass: process.env.GMAIL_PASSWORD,
-//   },
-// });
+//email authorization
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_USERNAME,
+    pass: process.env.GMAIL_PASSWORD,
+  },
+});
 
-// cron.schedule("* * * * *", function () {
-//   Event.find().then((events) => {
-//     let userId;
-//     console.log("event", events);
-//     console.log("new date", new Date());
-//     events.forEach((e) => {
-//       console.log("start day", typeof (e.start_date));
-//       let date = new Date(e.start_date).toISOString();
-//       let dateToCompareWithSecond = new Date();
-//       let dateToCompareWithout = moment(dateToCompareWithSecond).seconds(0).milliseconds(0).toISOString();
-//       console.log("outPut: dateToCompareWithout", dateToCompareWithout)
-//       // let dateToCompare = dateToCompareWithout.getTime()
-//       // console.log("outPut: dateToCompare", dateToCompare)
+cron.schedule("* * * * *", function () {
+  Event.find().then((events) => {
+    let userId;
+    console.log("event", events);
+    console.log("new date", new Date());
+    events.forEach((e) => {
+      console.log("start day", typeof (e.start_date));
+      let date = new Date(e.start_date).toISOString();
+      let dateToCompareWithSecond = new Date();
+      let dateToCompareWithout = moment(dateToCompareWithSecond).seconds(0).milliseconds(0).toISOString();
+      console.log("outPut: dateToCompareWithout", dateToCompareWithout)
 
-//       if (date == dateToCompareWithout) {
-//         console.log("event", e);
-//         userId = e._userId
-//       }
-//       return userId
-//     });
-//     console.log("userId", userId);
-//     return userId
-//   }).then((userId) => {
-//     return User.findOne({
-//       _id: userId
-//     })
-//   }).then((user) => {
-//     console.log("user", user);
-//     userEmail = user.email
-//     let mailOptions = {
-//       from: "ourmeetingapp@gmail.com",
-//       to: userEmail,
-//       subject: `Not a GDPR update ;)`,
-//       text: `Hi there, this email was automatically sent by us`
-//     };
-//     transporter.sendMail(mailOptions, function (error, info) {
-//       if (error) {
-//         throw error;
-//       } else {
-//         console.log("Email successfully sent!");
-//       }
-//     });
-//   })
+      if (date == dateToCompareWithout) {
+        console.log("event", e);
+        userId = e._userId
+      }
+    });
+    console.log("userId", userId);
+    userId
 
-// });
+    return User.findOne({
+      _id: userId
+    })
+  }).then((user) => {
+    console.log("user", user);
+    userEmail = user.email
+    let mailOptions = {
+      from: "ourmeetingapp@gmail.com",
+      to: userEmail,
+      subject: `reminder email`,
+      text: `Hi there, this email was automatically sent by us`
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        throw error;
+      } else {
+        console.log("Email successfully sent!");
+      }
+    });
+  })
+
+});
 
 module.exports = app;
