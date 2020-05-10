6a3d62642ef4856a4ac38592b2fad182c9ad435e
dkoushha <dimakoushha@gmail.com>
2020-05-10 08:26:14 +0200

    updating the function

--- meetingApp/routes/reminder.routes.js
+++ meetingApp/routes/reminder.routes.js
@@ -23,60 +23,63 @@ const transporter = nodemailer.createTransport({
 });
 
 cron.schedule("* * * * *", function () {
-    Event.find().then((events) => {
-            let userIds = []
-            events.forEach((e) => {
-                let date = moment.utc(e.start_date).seconds(0).milliseconds(0);
-                console.log("outPut: date", date)
-                // new Date(e.start_date).toISOString();
-                let dateToCompareWithSecond = new Date();
-                let dateToCompare = moment.utc(dateToCompareWithSecond).seconds(0).milliseconds(0);
-                console.log("outPut: dateToCompareWithout", dateToCompare)
-                if ((date.isSame(dateToCompare, "second") || date.isBefore(dateToCompare, "second")) && !e.sentReminder) {
-                    console.log("event remind", e.sentReminder);
-                    userIds.push(...e.attendList);
-                    e.sentReminder = true;
-                    console.log("event remind", e.sentReminder);
-                    e.save()
-                }
-            });
-            console.log("userIds", userIds);
-            let userIdsToUse = []
-            if (userIds.length >= 0) {
-                userIds.forEach((id) => {
-                    userIdsToUse.push(new mongoose.Types.ObjectId(id))
-                    return userIdsToUse
-                });
-                console.log("user id to use", userIdsToUse);
+    Event.find().populate("attendList").then((events) => {
+        console.log("event", events);
+        events.forEach((e) => console.log(e.attendList))
+        let userIds = []
+        events.forEach((e) => {
+            let date = moment.utc(e.start_date).seconds(0).milliseconds(0);
+            console.log("outPut: date", date)
+            let dateToCompareWithSecond = new Date();
+            let dateToCompare = moment.utc(dateToCompareWithSecond).seconds(0).milliseconds(0);
+            console.log("outPut: dateToCompareWithout", dateToCompare)
+            if ((date.isSame(dateToCompare, "second") || date.isBefore(dateToCompare, "second")) && !e.sentReminder) {
+                console.log("event remind", e.sentReminder);
+                userIds.push(...e.attendList);
+                e.sentReminder = true;
+                console.log("event remind", e.sentReminder);
+                e.save()
             }
-            return User.find({
-                _id: {
-                    $in: userIdsToUse
-                }
-            })
-        })
-        .then((users) => {
-            console.log("users", users);
-            let mailList = "";
-            users.forEach((user) => {
-                mailList += user.email + ','
-                console.log("userId", user._id);
-            })
-            console.log("mailList", mailList);
-            let mailOptions = {
-                from: "ourmeetingapp@gmail.com",
-                to: mailList,
-                subject: `reminder email`,
-                text: `Hi there, this email was automatically sent by us`
-            };
-            transporter.sendMail(mailOptions, function (error, info) {
-                if (error) {
-                    console.log("error", error);
-                } else {
-                    console.log("Email successfully sent!");
-                }
-            });
         });
-});
+    })
+})
+//     console.log("userIds", userIds);
+//     let userIdsToUse = []
+//     if (userIds.length >= 0) {
+//         userIds.forEach((id) => {
+//             userIdsToUse.push(new mongoose.Types.ObjectId(id))
+//             return userIdsToUse
+//         });
+//         console.log("user id to use", userIdsToUse);
+//     }
+//     return User.find({
+//         _id: {
+//             $in: userIdsToUse
+//         }
+//     })
+// })
+// .then((users) => {
+//     console.log("users", users);
+//     let mailList = "";
+//     users.forEach((user) => {
+//         mailList += user.email + ','
+//         console.log("userId", user._id);
+//     })
+//     console.log("mailList", mailList);
+//     let mailOptions = {
+//         from: "ourmeetingapp@gmail.com",
+//         to: mailList,
+//         subject: `reminder email`,
+//         text: `Hi there, this email was automatically sent by us`
+//     };
+//     transporter.sendMail(mailOptions, function (error, info) {
+//         if (error) {
+//             console.log("error", error);
+//         } else {
+//             console.log("Email successfully sent!");
+//         }
+//     });
+// });
+// });
 
 module.exports = router;
