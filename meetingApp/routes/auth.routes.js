b6190c25c05572d7570535140b67e19c097e86e7
Noubar Topalian <topalian.noubar@gmail.com>
2020-05-14 16:29:01 +0200

    verification tied to fix

--- meetingApp/routes/auth.routes.js
+++ meetingApp/routes/auth.routes.js
@@ -11,15 +11,12 @@ const bcryptSalt = 10;
 // Add passport
 const passport = require("passport");
 // add express-validation
-const {
-  validationResult
-} = require("express-validator");
+const { validationResult } = require("express-validator");
 // add middleware
 const signUpValidation = require("../helpers/middlewares").signUpValidation;
 // nodemailer
 const nodemailer = require("nodemailer");
-const axios = require('axios')
-
+const axios = require("axios");
 
 // email authorization
 const transporter = nodemailer.createTransport({
@@ -37,7 +34,7 @@ router.get("/", (req, res) => {
 
 // signup, signUpValidation in the helpers folder
 router.post("/signup", signUpValidation, (req, res) => {
-  // get the validation errors 
+  // get the validation errors
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -46,7 +43,7 @@ router.post("/signup", signUpValidation, (req, res) => {
     });
   }
 
-  // save new user 
+  // save new user
   const salt = bcrypt.genSaltSync(bcryptSalt);
   const hashPass = bcrypt.hashSync(req.body.password, salt);
   let user = new User({
@@ -55,69 +52,66 @@ router.post("/signup", signUpValidation, (req, res) => {
     image: `https://api.adorable.io/avatars/59/${req.body.email}`,
   });
   user.save();
-  // create new token for the user to verify its email 
+  // create new token for the user to verify its email
   const token = new Token({
     _userId: user._id,
     _eventId: user._id,
     token: randomToken(16),
   });
-  token.save();
-  // send a verification email
-  const mailOptions = {
-    from: "ourmeetingapp@gmail.com",
-    to: user.email,
-    subject: "Account Verification Token",
-    // check why it's not working
-    html: `<p>Hi there,<br></br>
+  token.save().then((token) => {
+    // send a verification email
+    const mailOptions = {
+      from: "ourmeetingapp@gmail.com",
+      to: user.email,
+      subject: "Account Verification Token",
+      // check why it's not working
+      html: `<p>Hi there,<br></br>
     To verify your email, simply click below.</p><br>
     <a href= "http://${req.headers.host}/confirmations/${token.token}">verify your email</a><br>
     <h4>Enjoy<br>
-    The Plan-It Team</h4>`
-  };
-  // render the res after signup
-  transporter.sendMail(mailOptions, function (err) {
-    if (err) {
-      return res.send({
-        msg: err.message,
-      });
-    }
-    let userEmail = req.body.email
-    res.render("auth/emailConfirmation", {
-      userEmail: userEmail
+    The Plan-It Team</h4>`,
+    };
+    // render the res after signup
+    transporter.sendMail(mailOptions, function (err) {
+      if (err) {
+        return res.send({
+          msg: err.message,
+        });
+      }
     });
   });
+  let userEmail = req.body.email;
+  res.render("auth/emailConfirmation", {
+    userEmail: userEmail,
+  });
 });
-
 
 //render personalAccount after email verification
 router.get("/confirmations/:token", (req, res) => {
   console.log(req.params);
   Token.findOne({
-      token: req.params.token,
-    })
+    token: req.params.token,
+  })
     .then((token) => {
-      console.log("outPut: token", token)
+      console.log("outPut: token", token);
       return User.findOne({
         _id: token._userId,
       });
     })
     .then((user) => {
-      console.log("outPut: user", user)
+      console.log("outPut: user", user);
       user.isVerified = true;
       return user.save();
     })
     .then((user) => {
-      req.login(user, () =>
-        res.redirect("/personalAccount")
-      );
+      req.login(user, () => res.redirect("/personalAccount"));
     });
 });
-
 
 //Login
 router.get("/login", (req, res) => {
   res.render("auth/signupForm", {
-    errorArr: req.flash("error")
+    errorArr: req.flash("error"),
   });
 });
 router.post(
@@ -133,13 +127,10 @@ router.post(
 //Logout
 router.get("/logout", (req, res) => {
   req.logOut();
-  let message = "Thank you for using our app"
+  let message = "Thank you for using our app";
   res.render("auth/signupForm", {
-    message: message
+    message: message,
   });
 });
-
-
-
 
 module.exports = router;
