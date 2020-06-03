42a62d3117e4a97360f3b5dd0e311aca219713bc
dkoushha <dimakoushha@gmail.com>
2020-06-03 14:41:14 +0200

    changes

--- meetingApp/routes/reminder.routes.js
+++ meetingApp/routes/reminder.routes.js
@@ -14,53 +14,53 @@ const cron = require("node-cron");
 const mongoose = require("mongoose");
 
 //email authorization
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.GMAIL_USERNAME,
-        pass: process.env.GMAIL_PASSWORD,
-    },
-});
+// const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//         user: process.env.GMAIL_USERNAME,
+//         pass: process.env.GMAIL_PASSWORD,
+//     },
+// });
 
-cron.schedule("* * * * *", function () {
-    Alarm.find()
-        .populate("_userId").populate("_eventId")
-        .then((response) => {
-            response.forEach((e) => {
-                let event = e._eventId;
-                let user = e._userId;
-                let reminderDate = moment.utc(e.remindTime).seconds(0).milliseconds(0);
-                let dateToCompare = moment
-                    .utc(new Date)
-                    .seconds(0)
-                    .milliseconds(0);
-                if (
-                    (reminderDate.isSame(dateToCompare, "second") ||
-                        reminderDate.isBefore(dateToCompare, "second")) &&
-                    !e.sentReminder
-                ) {
-                    let userEmail = user.email
-                    e.sentReminder = true;
-                    let eventDate = moment.utc(event.start_date).local().format("LLLL");
-                    let mailOptions = {
-                        from: "ourmeetingapp@gmail.com",
-                        to: userEmail,
-                        subject: `reminder email`,
-                        text: `Hi there, this email was automatically sent to you as 
-                        a reminder for the event name ${event.text} on ${eventDate}`
-                    };
-                    transporter.sendMail(mailOptions, function (error, info) {
-                        if (error) {
-                            console.log("error", error);
-                        } else {
-                            console.log("Email successfully sent!");
-                        }
-                    });
-                    e.save();
-                }
-            });
-        });
-});
+// cron.schedule("* * * * *", function () {
+//     Alarm.find()
+//         .populate("_userId").populate("_eventId")
+//         .then((response) => {
+//             response.forEach((e) => {
+//                 let event = e._eventId;
+//                 let user = e._userId;
+//                 let reminderDate = moment.utc(e.remindTime).seconds(0).milliseconds(0);
+//                 let dateToCompare = moment
+//                     .utc(new Date)
+//                     .seconds(0)
+//                     .milliseconds(0);
+//                 if (
+//                     (reminderDate.isSame(dateToCompare, "second") ||
+//                         reminderDate.isBefore(dateToCompare, "second")) &&
+//                     !e.sentReminder
+//                 ) {
+//                     let userEmail = user.email
+//                     e.sentReminder = true;
+//                     let eventDate = moment.utc(event.start_date).local().format("LLLL");
+//                     let mailOptions = {
+//                         from: "ourmeetingapp@gmail.com",
+//                         to: userEmail,
+//                         subject: `reminder email`,
+//                         text: `Hi there, this email was automatically sent to you as 
+//                         a reminder for the event name ${event.text} on ${eventDate}`
+//                     };
+//                     transporter.sendMail(mailOptions, function (error, info) {
+//                         if (error) {
+//                             console.log("error", error);
+//                         } else {
+//                             console.log("Email successfully sent!");
+//                         }
+//                     });
+//                     e.save();
+//                 }
+//             });
+//         });
+// });
 
 
 module.exports = router;
